e09cefae2fa63546d43da111e14667300cd9d2dc
N1ghtStorm <bushuevdv@mail.ru>
2022-01-17 01:52:12 +0300

    add get image names

--- src/images.rs
+++ src/images.rs
@@ -14,7 +14,34 @@ pub fn get_all_galleries() -> Vec<String>{
         vec.push(Path::new(&path.unwrap()
                                 .path()
                                 .display()
-                                .to_string()).file_name().unwrap().to_str().unwrap().to_string());
+                                .to_string()).file_name()
+                                    .unwrap()
+                                    .to_str()
+                                    .unwrap()
+                                    .to_string());
+    }
+
+    vec
+}
+
+pub fn get_all_gallery_image_names(gallery_name: String) -> Vec<String>{
+    let paths = fs::read_dir(format!("./images/{gallery_name}")).unwrap();
+    let mut vec = Vec::new();
+
+    for path in paths {
+        // vec.push(path.unwrap()
+        //                 .path()
+        //                 .display()
+        //                 .to_string());
+
+        vec.push(Path::new(&path.unwrap()
+                                .path()
+                                .display()
+                                .to_string()).file_name()
+                                    .unwrap()
+                                    .to_str()
+                                    .unwrap()
+                                    .to_string());
     }
 
     vec
--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .service(hi)
             .service(get_all_galleries)
+            .service(get_all_images_names)
     })
     .bind(url)?
     .run()
@@ -31,15 +32,13 @@ async fn hi() -> impl Responder {
 #[get("/api/galleries")]
 async fn get_all_galleries() -> impl Responder {
     let galleries = images::get_all_galleries();
-    let serialized = serde_json::to_string(&galleries).unwrap();
-    HttpResponse::Ok().body(serialized)
+    let serialized_names = serde_json::to_string(&galleries).unwrap();
+    HttpResponse::Ok().body(serialized_names)
 }
 
-#[get("/api/image_names/{gallery_name}")]
-async fn get_all_images_name(web::Path(gallery_name): web::Path<String>) -> impl Responder {
-
-
-    HttpResponse::Ok().body("
-        IMAGE GALLERY READY
-    ")
+#[get("/api/galleries/{gallery_name}")]
+async fn get_all_images_names(web::Path(gallery_name): web::Path<String>) -> impl Responder {
+    let images_names = images::get_all_gallery_image_names(gallery_name);
+    let serialized_names = serde_json::to_string(&images_names).unwrap();
+    HttpResponse::Ok().body(serialized_names)
 }
